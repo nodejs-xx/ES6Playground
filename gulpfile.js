94ef28d4cace416df1b106b51b24f5b45cc1745d
sl <sblig@126.com>
2016-09-09 15:20:29 +0800

    add lean example

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require("gulp");
 var shell = require("gulp-shell");
 
 gulp.task("default", callback => {
-  return gulp.watch("./src/*.js", event => {
-    var file = event.path;
-    var regexp = /[^\/]*$/;
-    console.log("run \"" + regexp.exec(file)[0]+"\": ");
-    gulp.src(file)
-    .pipe(shell(["node "+file],{ignoreErrors: true}));
-  });
+	return gulp.watch("./src/*.js", event => {
+		var file = event.path;
+		var regexp = /[^\/]*$/;
+		console.log("run \"" + regexp.exec(file)[0] + "\": ");
+		gulp.src(file)
+			.pipe(shell(["node " + file], {
+				ignoreErrors: true
+			}));
+	});
 });
---
+++ src/app-1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Liliang on 2016/9/8.
+ */
+"use strict"
+// for-of  
+/**
+ * for-in循环用来遍历对象属性。
+ * for-of循环用来遍历数据—例如数组中的值。
+ * for-of循环也可以遍历其它的集合
+ * for-of循环不仅支持数组，还支持大多数类数组对象，例如DOM NodeList对象。
+ * for-of循环也支持字符串遍历，它将字符串视为一系列的Unicode字符来进行遍历：
+ * for-of循环同样支持Map和Set对象遍历。
+ */
+// 數組
+var myArray = [1,2,3,6,7,9];
+for (var value of myArray){
+	console.log(value);
+}
+
